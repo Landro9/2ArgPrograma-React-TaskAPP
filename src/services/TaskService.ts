20fd4554f3687d10f9de138e1eb50fdc489054e1
Brenda Mollaret <mollaretbrenda@gmail.com>
2023-11-17 19:59:28 -0300

    renderTask

--- src/services/TaskService.ts
+++ src/services/TaskService.ts
@@ -1,6 +1,6 @@
 import { Task } from '../types/Task';
 
-const BASE_URL = 'https://twoback-taskapp.onrender.com';
+const BASE_URL = 'https://twoback-taskapp.onrender.com/tasks';
 
 export const TaskService = {
   
